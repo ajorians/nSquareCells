3235897c1b252a11adac29cbb31f4c8126fb44e9
A.J. Orians <ajorians@gmail.com>
2016-06-15 20:27:17 -0400

    Allowed the levels to be chosen.

--- src/Levels.c
+++ src/Levels.c
@@ -1,20 +1,46 @@
 #include "Levels.h"
 #include <string.h>
 
+#define LEVEL(x) case x: strcpy(pstr, LEVEL_ ## x ); break;
 void LevelLoad(char* pstr, int nLevelNum)
 {
    switch( nLevelNum ) {
-   case 1: strcpy(pstr, LEVEL_1); break;
-   case 2: strcpy(pstr, LEVEL_2); break;
-   case 3: strcpy(pstr, LEVEL_3); break;
-   case 4: strcpy(pstr, LEVEL_4); break;
-   case 5: strcpy(pstr, LEVEL_5); break;
-   case 6: strcpy(pstr, LEVEL_6); break;
-   case 7: strcpy(pstr, LEVEL_7); break;
-   case 8: strcpy(pstr, LEVEL_8); break;
-   case 9: strcpy(pstr, LEVEL_9); break;
-   case 19: strcpy(pstr, LEVEL_19); break;
-   default: strcpy(pstr, LEVEL_33); break;
+   LEVEL(1);
+   LEVEL(2);
+   LEVEL(3);
+   LEVEL(4);
+   LEVEL(5);
+   LEVEL(6);
+   LEVEL(7);
+   LEVEL(8);
+   LEVEL(9);
+   LEVEL(10);
+   LEVEL(11);
+   LEVEL(12);
+   LEVEL(13);
+   LEVEL(14);
+   LEVEL(15);
+   LEVEL(16);
+   LEVEL(17);
+   LEVEL(18);
+   LEVEL(19);
+   LEVEL(20);
+   LEVEL(21);
+   LEVEL(22);
+   LEVEL(23);
+   LEVEL(24);
+   LEVEL(25);
+   LEVEL(26);
+   LEVEL(27);
+   LEVEL(28);
+   LEVEL(29);
+   LEVEL(30);
+   LEVEL(31);
+   LEVEL(32);
+   LEVEL(33);
+   LEVEL(34);
+   LEVEL(35);
+   LEVEL(36);
    }
 }
 
